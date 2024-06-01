e493574cb411c191015e8450ad8f54350314ebca
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-01 09:08:47 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,25 @@
+
+public class Solution {
+
+    private static final int ALPHABET_SIZE = 26;
+
+    public int numberOfSpecialChars(String word) {
+        int[] lowerCaseAtLeastOnce = new int[ALPHABET_SIZE];
+        int[] upperCaseAtLeastOnce = new int[ALPHABET_SIZE];
+
+        for (char letter : word.toCharArray()) {
+            if (Character.isLowerCase(letter)) {
+                lowerCaseAtLeastOnce[letter - 'a'] = 1;
+            } else {
+                upperCaseAtLeastOnce[letter - 'A'] = 1;
+            }
+        }
+
+        int numberOfSpecialChars = 0;
+        for (int i = 0; i < ALPHABET_SIZE; ++i) {
+            numberOfSpecialChars += (lowerCaseAtLeastOnce[i] & upperCaseAtLeastOnce[i]);
+        }
+
+        return numberOfSpecialChars;
+    }
+}
