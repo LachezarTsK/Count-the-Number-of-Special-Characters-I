459d9f9251ba1e9b044584e85f9b3cab35238cc8
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-01 09:28:28 +0200

    Update Solution.cs

--- Solution.cs
+++ Solution.cs
@@ -24,7 +24,7 @@ public class Solution
         int numberOfSpecialChars = 0;
         for (int i = 0; i < ALPHABET_SIZE; ++i)
         {
-            numberOfSpecialChars += (lowerCaseAtLeastOnce[i] & upperCaseAtLeastOnce[i]);
+            numberOfSpecialChars += lowerCaseAtLeastOnce[i] & upperCaseAtLeastOnce[i];
         }
 
         return numberOfSpecialChars;
