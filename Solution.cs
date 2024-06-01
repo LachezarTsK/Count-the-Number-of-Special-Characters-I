0afd27c6a7f9358e31a7b233dfd8823473ea7b7c
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-01 09:11:43 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,32 @@
+
+using System;
+
+public class Solution
+{
+    static readonly int ALPHABET_SIZE = 26;
+    public int NumberOfSpecialChars(string word)
+    {
+        int[] lowerCaseAtLeastOnce = new int[ALPHABET_SIZE];
+        int[] upperCaseAtLeastOnce = new int[ALPHABET_SIZE];
+
+        foreach (char letter in word)
+        {
+            if (char.IsLower(letter))
+            {
+                lowerCaseAtLeastOnce[letter - 'a'] = 1;
+            }
+            else
+            {
+                upperCaseAtLeastOnce[letter - 'A'] = 1;
+            }
+        }
+
+        int numberOfSpecialChars = 0;
+        for (int i = 0; i < ALPHABET_SIZE; ++i)
+        {
+            numberOfSpecialChars += (lowerCaseAtLeastOnce[i] & upperCaseAtLeastOnce[i]);
+        }
+
+        return numberOfSpecialChars;
+    }
+}
